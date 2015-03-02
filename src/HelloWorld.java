043a986d79d74b289685f7cc324a330a9fea5631
Michael McMillan <email@michaelmcmillan.net>
2015-03-02 12:06:11 +0100

    Basic Authentication implemented as filter.

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,17 +1,31 @@
 import static spark.Spark.*;
+import java.util.Base64;
 
 public class HelloWorld {
     public static void main(String[] args) {
 
-        setPort(1339);
+        setPort(1999);
 
         before((request, response) -> {
 
-            System.out.println(request.headers());
+            String authHeader = request.headers("Authorization");
 
-            if (true) {
-                halt(401, "Du er ikke autentisert.");
-            }
+            if (authHeader == null
+            ||  authHeader.startsWith("Basic") == false
+            ||  authHeader.split(" ").length != 2)
+                halt(401, "Ugyldig format på autentiseringsstrengen.");
+            else
+                authHeader = authHeader.split(" ")[1];
+
+            byte[] decoded = Base64.getDecoder().decode(authHeader);
+            authHeader = new String(decoded, "UTF-8");
+            String[] authParts = authHeader.split(":");
+
+            String username = (String) authParts[0];
+            String password = (String) authParts[1];
+
+            if (!username.equals("mike") && !password.equals("hello"))
+                halt(401, "Feil brukernavn eller passord.");
         });
 
         get("/hello", (req, res) -> {
