a6f88a8c053d053b2620b026bfe96edab2db8cf9
sharklaks <sharklaks@gmail.com>
2015-03-04 10:57:43 +0100

    Ingenting a si

--- src/database/AppointmentsServletDao.java
+++ src/database/AppointmentsServletDao.java
@@ -4,22 +4,20 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.Statement;
-import java.util.ArrayList;
 
 /**
- * Created by sharklaks on 02/03/15.
+ * Created by sharklaks on 04/03/15.
  */
-public class TestServletDao implements  DbService {
-
+public class AppointmentsServletDao implements DbService {
 
     private Connection conn;
     private Statement stmt;
     private String uri;
     private static String   user = "sql368919",
-                            password = "zK8!iQ9!",
-                            dbName = "sql368919";
+            password = "zK8!iQ9!",
+            dbName = "sql368919";
 
-    public  TestServletDao(){
+    public  AppointmentsServletDao(){
         uri = "jdbc:mysql://sql3.freemysqlhosting.net:3306/" + dbName;
     }
 
@@ -29,16 +27,16 @@ public class TestServletDao implements  DbService {
     }
 
     @Override
-    public Object readOne(int id) {
+    public String readOne(int id) {
         try{
 
             conn = DriverManager.getConnection(uri, user, password);
             stmt = conn.createStatement();
 
-            ResultSet resultSet = stmt.executeQuery("select * from test limit 1");
+            ResultSet resultSet = stmt.executeQuery("select * from Appointment limit 1");
 
             while (resultSet.next()){
-                return resultSet.getString("test");
+                return resultSet.getString("tittel");
             }
 
         }catch (Exception e){
@@ -49,22 +47,6 @@ public class TestServletDao implements  DbService {
 
     @Override
     public String readAll() {
-        try{
-            ArrayList<String> arrayList = new ArrayList<>();
-
-            conn = DriverManager.getConnection(uri, user, password);
-            stmt = conn.createStatement();
-
-            ResultSet resultSet = stmt.executeQuery("select * from test limit 1");
-
-            while (resultSet.next()){
-                arrayList.add(resultSet.getString("test"));
-            }
-//            return arrayList;
-            return  null;
-        }catch (Exception e){
-            System.out.println("Database error: " + e.getMessage());
-        }
         return null;
     }
 
--- src/database/DbService.java
+++ src/database/DbService.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 /**
  * Created by sharklaks on 02/03/15.
  */
-public interface DbService<T> {
-    public boolean create(T entity);
-    public T readOne (int id);
-    public ArrayList<T> readAll();
-    public boolean update(int id, T newObject);
+public interface DbService {
+    public boolean create(String entity);
+    public String readOne (int id);
+    public String readAll();
+    public boolean update(int id, String newObject);
     public boolean delete(int id);
 }
