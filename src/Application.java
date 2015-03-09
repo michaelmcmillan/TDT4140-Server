469e6b668c7adedf241f421342182c64e0f307aa
jonaslaksen <jonaslaksen@live.com>
2015-03-09 15:33:47 +0100

    Kommentert ut auth

--- src/Application.java
+++ src/Application.java
@@ -17,19 +17,19 @@ public class Application {
         //setIpAddress("78.91.74.53");
         setPort(2009);
 
-        before((request, response) -> {
-            String authHeader = request.headers("Authorization");
-            Logger.console(authHeader);
-            Authentication authentication = new Authentication(authHeader);
-
-            try {
-                authentication.checkCredentials();
-            } catch (AuthenticationException authError) {
-                Logger.console(authError.getMessage(), request.ip());
-                halt(401, "Du er ikke autentisert.");
-            }
-
-        });
+//        before((request, response) -> {
+//            String authHeader = request.headers("Authorization");
+//            Logger.console(authHeader);
+//            Authentication authentication = new Authentication(authHeader);
+//
+//            try {
+//                authentication.checkCredentials();
+//            } catch (AuthenticationException authError) {
+//                Logger.console(authError.getMessage(), request.ip());
+//                halt(401, "Du er ikke autentisert.");
+//            }
+//
+//        });
 
         get("/", (req, res) -> {
             ArrayList<Person> folks = Person.getAll();
