7a965a0ec4241340238346724e6ad73fa469ec38
jonaslaksen <jonaslaksen@live.com>
2015-03-10 14:20:28 +0100

    Can updategit add src/database/ServletHelper.java

--- src/database/ServletHelper.java
+++ src/database/ServletHelper.java
@@ -63,11 +63,62 @@ public class ServletHelper {
     }
 
     public static boolean delete(String table, int id){
-        return true;
+        DatabaseConnection database = new DatabaseConnection();
+        String delete = "DELETE FROM " +  table + " WHERE id=" + id;
+        try{
+            PreparedStatement preparedStatement = database.getConn().prepareStatement(delete);
+            preparedStatement.execute();
+            return true;
+        }catch (SQLException error){
+            Logger.console(error.getMessage());
+        }
+        return false;
     }
 
     public static boolean update(String table, HashMap<String, Object> map){
-        return true;
+        int id = (Integer)map.get("id");
+        map.remove("id");
+        DatabaseConnection database = new DatabaseConnection();
+
+
+        ArrayList<Object> values = new ArrayList<>();
+        String fields = "";
+
+        for (Map.Entry<String, Object> row: map.entrySet()){
+            fields += row.getKey() + "=?,";
+            values.add(row.getValue());
+        }
+        if (fields.length() > 0){
+            fields = fields.substring(0, fields.length() - 1);
+        }
+
+
+        String update = "UPDATE " + table + " SET " + fields + " WHERE id=" + id;
+        try{
+            PreparedStatement preppedStatement = null;
+            preppedStatement = database.getConn().prepareStatement(update, Statement.RETURN_GENERATED_KEYS);
+
+            int count = 1;
+            for (Object value: values){
+                if (value.getClass().getName() == "java.lang.String")   preppedStatement.setString( count, (String)values.get(count - 1));
+                else {
+                    int tempInt = (Integer) values.get(count - 1);
+                    if (tempInt > 0)
+                        preppedStatement.setInt(count, (Integer) values.get(count - 1));
+                    else
+                        preppedStatement.setNull(count, Types.INTEGER);
+
+                }
+
+                count ++;
+            }
+            preppedStatement.execute();
+            return true;
+        }catch (SQLException error){
+            Logger.console(error.getMessage());
+        }
+
+        return false;
     }
 
 }
